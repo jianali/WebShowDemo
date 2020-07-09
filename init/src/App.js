11ff1f82c62aa8fa7b6742521adca692660f6a02
jianali <jian.a.li@transwarp.io>
2020-07-09 16:21:41 +0800

    modify README file

--- init/src/App.js
+++ init/src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//加入antd样式及控件
+import { Button, DatePicker, version } from 'antd'
+import 'antd/dist/antd.css'
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +24,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>antd version is:{version}</h1>
+      <DatePicker />
+      <Button type="primary" style={{ marginLeft: 8 }}>
+        Primary Button
+      </Button>
     </div>
   );
 }
