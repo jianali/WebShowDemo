049153608b8c25d74fa131674ef4705cff2f70d2
jianali <jian.a.li@transwarp.io>
2020-07-10 11:29:53 +0800

    集成antd的demo修改

--- init/src/App.js
+++ init/src/App.js
@@ -3,14 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 //加入antd样式及控件
-import { Button, DatePicker, version } from 'antd'
+import { Button, DatePicker, version, Divider } from 'antd'
 import 'antd/dist/antd.css'
+
+//使用栅格系统布局
+import { Row, Col } from 'antd'
+//页面布局
+import { Layout } from 'antd'
 
 
 
 function App() {
+
+  const style = {background: '#0092ff', padding:'8px 0'};
+  const { Header ,Footer,Sider,Content} = Layout;
   return (
     <div className="App">
+      <div className="space-align-container">
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -30,6 +39,27 @@ function App() {
       <Button type="primary" style={{ marginLeft: 8 }}>
         Primary Button
       </Button>
+      {/* 使用layout进行大组件的布局 */}
+      <Layout>
+          <header>Header</header>
+          <Layout>
+            <Sider>Silder</Sider>
+            <Content>
+                {/* 使用栅格系统来进行小组件的布局 */}
+                <Row>
+                  <Col span={24} style={style}>col</Col>
+                </Row>
+                <Divider></Divider>
+                <Row>
+                  <Col span={12} style={style}>col</Col>
+                </Row>
+                <Row>
+                  <Col span={6} offset={3} className="col_style">col</Col>
+                </Row>
+            </Content>
+          </Layout>
+      </Layout>
+      </div>
     </div>
   );
 }
